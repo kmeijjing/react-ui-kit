9902cb902ba6dcdb2a8437cbb763f0d6baea7f16
eunjin0212 <eunjin9639@gmail.com>
2024-08-15 20:17:59 +0900

    🔧 tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,15 @@ const config: Config = {
  content: [
   "index.html",
   "./src/**/*.{html,js,ts,tsx,jsx,css,mdx}",
-  './.storybook/**/*.{js,ts,jsx,tsx,mdx}',
-  './stories/**/*.{js,ts,jsx,tsx,mdx}',
+  // './.storybook/**/*.{js,ts,jsx,tsx,mdx}',
  ],
+ safelist: Object.values(colors).flatMap((color) => [
+  `bg-[${color}]`,
+  `text-[${color}]`,
+  `border-[${color}]`,
+  `decoration-[${color}]`,
+  `hover:before:bg-[${color}]`,
+]),
  mode: 'jit',
  important: true,
  theme: {
@@ -17,13 +23,17 @@ const config: Config = {
     pretendard: ["Pretendard"],
    },
    spacing: {
-    '0.5': '0.125rem'
+    '0.5': '0.125rem', // 2px
+    '1.5': '0.375rem', // 6px
+    '2.5': '0.625rem', // 10px
+    '3.5': '0.875rem', // 14px
+    '4.5': '1.125rem', // 20px
    },
    borderRadius: {
     4: '0.25rem',
-   }
+   },
+   colors,
   },
-  colors,
  },
  plugins: [],
 } satisfies Config;
