e3e53973ca4e89e814c17fa5367a40aa05c44fad
eunjin0212 <eunjin9639@gmail.com>
2024-08-27 21:00:13 +0900

    🍱 Organize icon

---
+++ src/assets/ArrowDownIcon.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from 'react';
+
+export const ArrowDown24 = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		width='24'
+		height='24'
+		viewBox='0 0 24 24'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+  {...props}
+	>
+		<path
+			d='M21.2 7.5L12.1748 16.329C12.0777 16.424 11.9224 16.424 11.8252 16.329L2.80001 7.5'
+			stroke='currentColor'
+			strokeWidth='1.5'
+			strokeLinecap='round'
+		/>
+	</svg>
+);
---
+++ src/assets/MinusIcon.tsx
@@ -0,0 +1,39 @@
+import { SVGProps } from 'react';
+
+export const MinusIcon8 = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		width='8'
+		height='8'
+		viewBox='0 0 8 8'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+  {...props}
+	>
+		<path
+			d='M0.93335 4H7.06668'
+			stroke='currentColor'
+			stroke-width='0.6'
+			strokeLinecap='round'
+		/>
+	</svg>
+);
+
+export const MinusIcon12 = (props: SVGProps<SVGSVGElement>) => {
+	return (
+		<svg
+			width='12'
+			height='12'
+			viewBox='0 0 12 12'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+  {...props}
+		>
+			<path
+				d='M1.40002 6H10.6'
+				stroke='currentColor'
+    strokeWidth='1.25'
+				strokeLinecap='round'
+			/>
+		</svg>
+	);
+};
