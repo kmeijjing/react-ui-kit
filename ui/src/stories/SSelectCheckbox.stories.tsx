620e39c4b3c369b7b65494c6ee408254886d4a35
MeeKyeong Kim <kmeijjing@gmail.com>
2024-11-25 23:15:11 +0900

    🎨 add sSelectCheckbox storybook

---
+++ ui/src/stories/SSelectCheckbox.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SSelectCheckbox from '../components/SSelectCheckbox';
+
+const meta = {
+	title: 'SSelectCheckbox',
+	component: SSelectCheckbox,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof SSelectCheckbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const options = [
+	{ label: '옵션 1', value: '1' },
+	{ label: '옵션 2', value: '2' },
+	{ label: '옵션 3', value: '3' },
+	{ label: '긴 텍스트 옵션 4입니다', value: '4' },
+];
+
+export const Default: Story = {
+	args: {
+		options,
+		value: [],
+		onChange: (option) => console.log('선택된 옵션:', option),
+		placeholder: '선택해주세요',
+		className: 'w-200pxr',
+	},
+};
+
+export const WithLabel: Story = {
+	args: {
+		...Default.args,
+		label: '라벨',
+	},
+};
+
+export const WithSelectedValue: Story = {
+	args: {
+		...Default.args,
+		value: [options[0], options[1]],
+	},
+};
+
+export const Disabled: Story = {
+	args: {
+		...Default.args,
+		disabled: true,
+	},
+};
+
+export const ManyOptions: Story = {
+	args: {
+		...Default.args,
+		options: [
+			...options,
+			{ label: '옵션 5', value: '5', disable: true },
+			{ label: '옵션 6', value: '6' },
+			{ label: '옵션 7', value: '7' },
+			{ label: '옵션 8', value: '8' },
+			{ label: '옵션 9', value: '9' },
+			{ label: '옵션 10', value: '10' },
+			{ label: '옵션 5', value: '5' },
+			{ label: '옵션 6', value: '6' },
+			{ label: '옵션 7', value: '7' },
+			{ label: '옵션 8', value: '8' },
+			{ label: '옵션 9', value: '9' },
+			{ label: '옵션 10', value: '10' },
+		],
+	},
+};
